89c64c57841b69b224132000849d0590095f0720
DaviPMello27 <davipmello@hotmail.com>
2019-10-25 16:59:03 -0300

    Updated Double-Pass.py

--- Double-Pass.py
+++ Double-Pass.py
@@ -19,7 +19,7 @@ def fisrtPass(image):
                 result[y, x] = 0
             else:
                 result[y, x] = 255
-    showImage("Original image", cv2.cvtColor(result, cv2.COLOR_GRAY2BGR), 131)
+    showImage("Original image", cv2.cvtColor(result, cv2.COLOR_GRAY2BGR), 121)
     for y in range(1, image.shape[0]):
         for x in range(1, image.shape[1]):
             north = result[y - 1, x]
@@ -35,13 +35,32 @@ def fisrtPass(image):
                     result[y, x] = result[y - 1, x]
                 else:
                     result[y, x] = min(north, west)
-                    if(not(max(north, west) in links[min(north, west) - 1])):
-                        links[min(north, west) - 1].append(max(north, west))
+                    for i in range(len(links[max(north, west) - 1])):
+                        if(not(links[max(north, west) - 1][i] in links[min(north, west) - 1])):
+                            links[min(north, west) - 1].append(links[max(north, west) - 1][i])
+                    links[max(north, west) - 1][0] = min(north, west)
             else:
                 result[y, x] = 0
-    print(links)
-    return result
+    for i in range(len(links) - 1, 0, -1):
+        if(links[i][0] != i + 1):
+            links.pop(i)
+    #print(links)
+    return result, links
+
+def secondPass(image, links):
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            for i in range(len(links)):
+                for j in range(len(links[i])):
+                    image[image == links[i][j]] = links[i][0] * 10
+    return image, links
 
 img = cv2.cvtColor(cv2.imread("img/doublepass.jpeg"), cv2.COLOR_BGR2GRAY)
-showImage("Labelled image", cv2.cvtColor(fisrtPass(img), cv2.COLOR_GRAY2BGR), 132)
+img, links = fisrtPass(img)
+print(img)
+print(links)
+img, links = secondPass(img, links)
+print(img)
+print(links)
+showImage("labelled", cv2.cvtColor(img, cv2.COLOR_GRAY2BGR), 122)
 plt.show()
