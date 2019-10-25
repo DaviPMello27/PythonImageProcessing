7e4f7b90784220a4534d954e4782a774ddcd1e95
DaviPMello27 <davipmello@hotmail.com>
2019-10-25 17:33:35 -0300

    Updated Double-Pass.py and img/twenty.jpeg

--- Double-Pass.py
+++ Double-Pass.py
@@ -11,14 +11,11 @@ def showImage(title, image, pos = 111, effect = None):
 
 def fisrtPass(image):
     result = np.array(image)
+    result[result < 128] = 0
+    result[result >= 128] = 255
     currentLabel = 0
     links = []
-    for y in range(image.shape[0]):
-        for x in range(image.shape[1]):
-            if(result[y, x] < 128):
-                result[y, x] = 0
-            else:
-                result[y, x] = 255
+    
     showImage("Original image", cv2.cvtColor(result, cv2.COLOR_GRAY2BGR), 121)
     for y in range(1, image.shape[0]):
         for x in range(1, image.shape[1]):
@@ -44,23 +41,23 @@ def fisrtPass(image):
     for i in range(len(links) - 1, 0, -1):
         if(links[i][0] != i + 1):
             links.pop(i)
-    #print(links)
     return result, links
 
 def secondPass(image, links):
+    result = np.array(image)
     for y in range(image.shape[0]):
         for x in range(image.shape[1]):
             for i in range(len(links)):
                 for j in range(len(links[i])):
-                    image[image == links[i][j]] = links[i][0] * 10
-    return image, links
+                    if(image[y, x] == links[i][j]):
+                        result[y, x] = links[i][0] * 10 + 50
+    return result, links
 
 img = cv2.cvtColor(cv2.imread("img/doublepass.jpeg"), cv2.COLOR_BGR2GRAY)
 img, links = fisrtPass(img)
 print(img)
-print(links)
 img, links = secondPass(img, links)
 print(img)
 print(links)
-showImage("labelled", cv2.cvtColor(img, cv2.COLOR_GRAY2BGR), 122)
+showImage("Labelled", cv2.cvtColor(img, cv2.COLOR_GRAY2BGR), 122)
 plt.show()
