8cbcc14bba4f3f25de7d960d5f4a23cf082db3d3
DaviPMello27 <davipmello@hotmail.com>
2019-10-28 18:09:28 -0300

    Changed Double-Pass8c.py

--- Double-Pass8c.py
+++ Double-Pass8c.py
@@ -17,28 +17,28 @@ def fisrtPass(image):
     links = []
     
     showImage("Original image", cv2.cvtColor(result, cv2.COLOR_GRAY2BGR), 121)
-    for y in range(1, image.shape[0]):
-        for x in range(1, image.shape[1]):
-            north = result[y - 1, x]
-            west = result[y, x - 1]
+    for y in range(1, image.shape[0] - 1):
+        for x in range(1, image.shape[1] - 1):
             
+            directions = [result[y, x - 1], result[y - 1, x - 1], result[y - 1, x], result[y - 1, x + 1]]
+            directions = np.array(directions)
+            west, northwest, north, northeast = directions
             if(result[y, x] > 250):
-                if(north == 0 and west == 0):
+                if(west == northwest == north == northeast == 0):
                     currentLabel += 1
                     links.append([currentLabel])
                     result[y, x] = currentLabel
-                elif(north == 0 and west != 0):
-                    result[y, x] = result[y, x - 1]
-                elif(north != 0 and west == 0 or north == west):
-                    result[y, x] = result[y - 1, x]
+                elif(0 in directions):
+                    result[y, x] = min(directions[directions > 0])
+                    for i in range(4):
+                        for j in range(len(links[directions[i] - 1])):
+                            if(not(links[directions[i] - 1][j] in links[min(directions) - 1])):
+                                links[min(directions) - 1].append(links[directions[i]][j])
                 else:
-                    result[y, x] = min(north, west)
-                    for i in range(len(links[max(north, west) - 1])):
-                        if(not(links[max(north, west) - 1][i] in links[min(north, west) - 1])):
-                            links[min(north, west) - 1].append(links[max(north, west) - 1][i])
-                    links[max(north, west) - 1][0] = min(north, west)
+                    result[y, x] = min(directions)
             else:
                 result[y, x] = 0
+    print(links)
     for i in range(len(links) - 1, 0, -1):
         if(links[i][0] != i + 1):
             links.pop(i)
@@ -52,18 +52,8 @@ def secondPass(image, links):
         for x in range(image.shape[1]):
             for i in range(len(links)):
                 if(result[y, x] in links[i]):
-                    result[y, x] = (i+2)*36
+                    result[y, x] = (i+1)*10
     return result, links
-
-# def secondPass(image, links):
-#     result = np.array(image)
-#     for y in range(image.shape[0]):
-#         for x in range(image.shape[1]):
-#             for i in range(len(links)):
-#                 for j in range(len(links[i])):
-#                     if(image[y, x] == links[i][j]):
-#                         result[y, x] = links[i][0] * 10 + 50
-#     return result, links
 
 img = cv2.cvtColor(cv2.imread("img/doublepass.jpeg"), cv2.COLOR_BGR2GRAY)
 img, links = fisrtPass(img)
@@ -71,3 +61,14 @@ img, links = secondPass(img, links)
 print(img)
 showImage("Labelled", cv2.cvtColor(img, cv2.COLOR_GRAY2BGR), 122)
 plt.show()
+
+
+
+
+
+
+
+# for i in range(len(links[max(north, west) - 1])):
+#     if(not(links[max(north, west) - 1][i] in links[min(north, west) - 1])):
+#         links[min(north, west) - 1].append(links[max(north, west) - 1][i])
+# links[max(north, west) - 1][0] = min(north, west)
